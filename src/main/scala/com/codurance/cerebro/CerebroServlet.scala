6210dd9346f839d32754f266ea620be146983946
Sandro Mancuso <sandromancuso@yahoo.com>
2014-07-07 12:10:40 +0100

    Authorise users that belong to codurance.com

--- src/main/scala/com/codurance/cerebro/CerebroServlet.scala
+++ src/main/scala/com/codurance/cerebro/CerebroServlet.scala
@@ -2,6 +2,7 @@ package com.codurance.cerebro
 
 import java.net.URL
 import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletResponse.{SC_OK, SC_UNAUTHORIZED}
 import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeTokenRequest, GoogleTokenResponse}
 import com.google.api.client.json.jackson.JacksonFactory
 import com.google.api.client.http.javanet.NetHttpTransport
@@ -44,22 +45,19 @@ class CerebroServlet extends CerebroStack {
 
 	post("/authenticate") {
 		val authCode: String = params.getOrElse("authCode", halt(400))
-		println("authCode: " + authCode)
 		val tokenResponse: GoogleTokenResponse =
 			new GoogleAuthorizationCodeTokenRequest(
 				TRANSPORT, JSON_FACTORY, CLIENT_ID, CLIENT_SECRET, authCode, "postmessage"
 			).execute
-		println("tokenResponse: " + tokenResponse)
-		println("******** ACCESS TOKEN *******: " + tokenResponse.getAccessToken)
 		request.getSession.setAttribute("token", tokenResponse.toString)
-		println("*** authenticate")
 		val url = new URL("https://www.googleapis.com/plus/v1/people/me?fields=aboutMe%2Ccover%2FcoverPhoto%2CdisplayName%2Cdomain%2Cemails%2Clanguage%2Cname&access_token=" + tokenResponse.getAccessToken)
 		val userInfo = Await.result(GET(url).apply, 10.seconds) //this will throw if the response doesn't return within 1 second
-		println("userInfo: " + userInfo.bodyString)
-//		response.setStatus(HttpServletResponse.SC_OK)
-		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED)
+		val user = GooglePlusJSONResponseToUser.toUser(userInfo.bodyString)
+		user.domain match {
+			case Some(Domain("codurance.com")) => response.setStatus(SC_OK)
+			case _ => response.setStatus(SC_UNAUTHORIZED)
+		}
 		response.getWriter.print(new Gson().toJson("Successfully connected user: " + tokenResponse.getAccessToken))
-
 	}
 
 
